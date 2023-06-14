dae02d235ec7a3dd601c36b2595aafcac61a7d69
Sahib Kullar <123601618+s6hib@users.noreply.github.com>
2023-06-13 17:34:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,36 @@
 #include <vector>
 #include <sstream>
 #include <random>
+
 using namespace std;
 
 vector<string> stoicQuotes;
 
-string genStoicQuote(int quote) {
-    return stoicQuotes.at(quote);
+// Function to read quotes from file
+void readQuotesFromFile(const string& fileName) {
+    ifstream file(fileName);
+    if (!file) throw runtime_error("The file name does not exist!\n");
+
+    string line;
+    while (getline(file, line)) {
+        stoicQuotes.push_back(line);
+    }
+}
+
+// Function to generate a random number within the range of quotes
+int getRandomQuoteIndex() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(0, stoicQuotes.size() - 1);
+    return distrib(gen);
 }
 
 int main() {
     string fileName = "stoicQuotes.txt";
-    ifstream file(fileName);
-    if (!file) throw runtime_error("The file name does not exist!\n");
-    while (file) {
-        string s;
-        getline(file, s);
-        if (!file) break;
-        stoicQuotes.push_back(s);
-    }
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distrib(0, stoicQuotes.size() - 1);
-    int whichQuote = distrib(gen);
-    cout << genStoicQuote(whichQuote) << endl;
+    readQuotesFromFile(fileName);
+
+    int quoteIndex = getRandomQuoteIndex();
+    cout << stoicQuotes.at(quoteIndex) << endl;
+
     return 0;
 }
